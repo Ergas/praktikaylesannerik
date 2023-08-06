68aa4fbfcd0542c1f409e43f38cb3475591fe73b
Ergas <38925896+Ergas@users.noreply.github.com>
2023-08-06 16:56:12 +0300

    Add event design finished

--- solution/praktikaylrik/praktikaylrik/Pages/AddEvent.cshtml.cs
+++ solution/praktikaylrik/praktikaylrik/Pages/AddEvent.cshtml.cs
@@ -3,11 +3,11 @@ using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace praktikaylrik.Pages
 {
-    public class PrivacyModel : PageModel
+    public class AddEvent : PageModel
     {
-        private readonly ILogger<PrivacyModel> _logger;
+        private readonly ILogger<AddEvent> _logger;
 
-        public PrivacyModel(ILogger<PrivacyModel> logger)
+        public AddEvent(ILogger<AddEvent> logger)
         {
             _logger = logger;
         }
