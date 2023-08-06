7d39df115359be2cd7c80061b164fae1bd890790
Ergas <38925896+Ergas@users.noreply.github.com>
2023-08-06 17:38:56 +0300

    Design nearly finished
    Design still has some flaws which will be fixed if I will have enough time before submitting my work

--- solution/praktikaylrik/praktikaylrik/Pages/Participants.cshtml.cs
+++ solution/praktikaylrik/praktikaylrik/Pages/Participants.cshtml.cs
@@ -6,15 +6,15 @@ namespace praktikaylrik.Pages
 {
     [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
     [IgnoreAntiforgeryToken]
-    public class ErrorModel : PageModel
+    public class Participants : PageModel
     {
         public string? RequestId { get; set; }
 
         public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
 
-        private readonly ILogger<ErrorModel> _logger;
+        private readonly ILogger<Participants> _logger;
 
-        public ErrorModel(ILogger<ErrorModel> logger)
+        public Participants(ILogger<Participants> logger)
         {
             _logger = logger;
         }
